6ff4863a62123aaeab882bd804a39e633defeaa5
David M. Lemcoe Jr <david@Davids-MacBook-Pro.local>
2012-11-07 12:19:07 -0500

    First round of changes to make views utilize Twitter Bootstrap.

--- application/views/auth/general_message.php
+++ application/views/auth/general_message.php
@@ -1,1 +1,20 @@
-<?php echo $message; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="http://assets.delcoe.com/bootstrap.css" rel="stylesheet">
+<title>Notice</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="span6 offset3">
+            <div class="well">
+				<center>
+                	<h3><?php echo $message; ?></h3>
+				</center>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- application/views/auth/unregister_form.php
+++ application/views/auth/unregister_form.php
@@ -5,13 +5,34 @@ $password = array(
 	'size'	=> 30,
 );
 ?>
-<?php echo form_open($this->uri->uri_string()); ?>
-<table>
-	<tr>
-		<td><?php echo form_label('Password', $password['id']); ?></td>
-		<td><?php echo form_password($password); ?></td>
-		<td style="color: red;"><?php echo form_error($password['name']); ?><?php echo isset($errors[$password['name']])?$errors[$password['name']]:''; ?></td>
-	</tr>
-</table>
-<?php echo form_submit('cancel', 'Delete account'); ?>
-<?php echo form_close(); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="bootstrap.css" rel="stylesheet">
+        <title>Delete Account</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="span6 offset3">
+                    <div class="well">
+                        <center>
+							<h2>Delete Account</h2>
+                            <?php echo form_open($this->uri->uri_string()); ?>
+                            <div class="control-group">
+                                <?php echo form_label('Password', $password['id'], array('class' => 'control-label')); ?>
+                                <div class="controls">
+                                    <?php echo form_error('password'); ?>                                
+                                    <?php echo form_input($password); ?>
+                                    <p class="help-block"></p>
+                                </div>
+                            </div>
+                            <?php echo form_submit('cancel', 'Delete Account', 'class="btn btn-danger"'); ?>
+                            <?php echo form_close(); ?>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
